8da4a07ea95c979df044a1f086c7ce7ae6a012b0
Podter <48936015+Podter@users.noreply.github.com>
2022-05-12 15:52:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,74 @@
 #!/usr/bin/env node
+// imports and args
 import { createSpinner } from "nanospinner";
 import fs from 'fs-extra'
 import path from "path"
-import { exec } from "child_process";
-import { fileURLToPath } from 'url'
+import { execSync } from "child_process";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 import getDir from "./functions/getDir.js"
 import getName from "./functions/getName.js"
 import checkDir from "./functions/checkDir.js";
-import askServerPackages from "./functions/askServerPackages.js";
-import { addKnitToWally, addServerPackages, setProjectName } from "./functions/changeFiles.js"
+import setProjectName from "./functions/setProjectName.js"
+import getTemplate from "./functions/getTemplate.js";
 
-const templateDir = path.join(__dirname, "template")
 const args = process.argv.slice(2);
 const sleep = (ms = 500) => new Promise((r) => setTimeout(r, ms))
 
+// ask for project name and template
 const spinner = createSpinner("Creating project")
 const projectName = await getName(args)
 const projectDir = await getDir(args, projectName)
-if (await checkDir(projectDir) === true) {
+if (checkDir(projectDir) === true) {
     spinner.error({ text: `Directory ${projectDir} already exists.` });
     process.exit(1)
 }
-const isServerPackages = await askServerPackages()
+const template = await getTemplate()
+
+// start
 spinner.start()
-
 await sleep()
-fs.mkdirSync(projectDir)
-fs.copySync(path.join(templateDir, "default"), projectDir)
+
+// git clone template
+try {
+    execSync(`git clone https://github.com/Podter/knit-init.git -b ${template} ${projectDir}`, { stdio: 'pipe' })
+} catch (err) {
+    spinner.error({ text: `Error:\n${err}\nIs git installed?` });
+    process.exit(1)
+}
+fs.rmSync(path.join(projectDir, '.git'), { recursive: true, force: true })
+
+// set project name and username
 spinner.update({ text: "Setting up" })
-
+await setProjectName(projectDir, projectName, spinner)
 await sleep()
-exec(`cd ${projectDir} && foreman install && wally init`, async (err) => {
-    if (err) {
-        spinner.error({ text: `Error:\n${err}\nIs foreman and wally installed?` });
-        process.exit(1);
-    }
 
-    addKnitToWally(projectDir, spinner)
-    
-    spinner.update({ text: "Installing Knit" })
-    await sleep()
-    exec(`cd ${projectDir} && wally install`, async (err) => {
-        if (err) {
-            spinner.error({ text: `Error:\n${err}` });
-            process.exit(1);
-        }
+// foreman install
+try {
+    execSync(`cd ${projectDir} && foreman install`, { stdio: 'pipe' })
+} catch (err) {
+    spinner.error({ text: `Error:\n${err}\nIs foreman installed?` });
+    process.exit(1)
+}
 
-        spinner.update({ text: "Finishing up" })
-        await sleep()
-        if (isServerPackages === true) {
-            fs.copySync(path.join(templateDir, "withServerPackages", "default.project.json"), path.join(projectDir, "default.project.json"))
-            addServerPackages(projectDir, spinner)
-            fs.mkdirSync(path.join(projectDir, "ServerPackages"))
-        }
-        fs.renameSync(path.join(projectDir, "_gitignore"), path.join(projectDir, ".gitignore"))
-        fs.mkdirSync(path.join(projectDir, "src", "ReplicatedStorage"))
-        fs.mkdirSync(path.join(projectDir, "src", "ServerStorage"))
-        setProjectName(projectDir, projectName, spinner)
+// wally install
+spinner.update({ text: "Installing Knit" })
+await sleep()
+try {
+    execSync(`cd ${projectDir} && wally install`, { stdio: 'pipe' })
+} catch (err) {
+    spinner.error({ text: `Error:\n${err}\nIs wally installed?` });
+    process.exit(1)
+}
 
-    })
-})
+// some mkdirs
+spinner.update({ text: "Finishing up" })
+await sleep()
+fs.mkdirSync(path.join(projectDir, "src", "ReplicatedStorage"))
+fs.mkdirSync(path.join(projectDir, "src", "ServerStorage"))
+if (template === "template-server-packages") {
+    fs.mkdirSync(path.join(projectDir, "ServerPackages"))
+}
+
+// done and exit
+spinner.success({ text: "Done!" });
+process.exit(0)
