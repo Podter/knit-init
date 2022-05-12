ef383e7331428870de71decaaf2d90fd80d3d17f
Podter <48936015+Podter@users.noreply.github.com>
2022-05-12 15:52:54 +0700

    deleted template

--- template/default/src/StarterPlayerScripts/KnitClient.client.lua
+++
@@ -1,6 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Knit = require(ReplicatedStorage.Packages.Knit)
-
-Knit.Start():andThen(function()
-    print("Knit client started")
-end):catch(warn)
