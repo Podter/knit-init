e994fb318679ac7c1f4f3ac632ebd141b6578470
Podter <48936015+Podter@users.noreply.github.com>
2022-05-12 12:57:49 +0700

    init

--- src/functions/checkDir.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs-extra";
-
-export default async function checkDir(dir: fs.PathLike) {
-    if (fs.existsSync(dir)) {
-        return true
-    } else {
-        return false
-    }
-}
--- src/functions/getDir.ts
+++
@@ -1,9 +0,0 @@
-import path from "path"
-
-export default async function getDir(args: any, projectName: string) {
-    if (args[1]) {
-        return args[1]
-    } else {
-        return path.join(process.cwd(), projectName)
-    }
-}
